2baae69e3b19a78ea0a63af7de449789b95e63d4
Tojammel Hoque <tojammelhoque375@gmail.com>
2025-07-05 19:42:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+function App() {
+  return <div className="flex-center h-screen">App</div>;
+}
+
+export default App;
