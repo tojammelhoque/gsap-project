74877ffd9412db2ec02c440da33291b6d71cf39d
Tojammel Hoque <tojammelhoque375@gmail.com>
2025-07-06 17:46:33 +0530

    complete navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "./components/Navbar";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 function App() {
-  return <div className="flex-center h-screen">App</div>;
+  return (
+    <main>
+      <Navbar />
+    </main>
+  );
 }
 
 export default App;
